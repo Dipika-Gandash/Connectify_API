c7f1076e51c445405b280c0958ef0395a6ec130b
Dipika Gandash <dipikadevv1445@gmail.com>
2025-06-19 16:56:07 +0530

    setup MongoDB connection and environment configuration

--- src/app.js
+++ src/app.js
@@ -1,32 +1,15 @@
-const express = require('express');
-
+const express = require("express");
+const connectDB = require("./config/database");
+require('dotenv').config();
 const app = express();
 
-app.get('/sales', (err, req, res, next) => {
-    const {coupenCode} = req.query;
-    const salesCoupen = 'SALE50';
-    if(coupenCode != salesCoupen){
-     const err = new Error('Invalid coupen code')
-     next(err);
-    }
-    else{
-     next();
-    }
-})
-
-app.get('/sales', (req, res, next) => {
-     console.log('Congrulations! You have successfully applied the coupen code');
-     next();
-})
-
-app.get('/sales', (req, res) => {
-     res.send('Welcome to the sales page');
-})
-
-app.use((err, req, res, next) => {
-     res.status(500).send(`Error : ${err.message}`);
-})
-app.listen(3000, () => {
-     console.log('Server is running on port 3000');
-})
-
+connectDB()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(3000, () => {
+      console.log("Server is running on http://localhost:3000");
+    });
+  })
+  .catch((error) => {
+    console.error("Database connection failed :", error.message);
+  });
