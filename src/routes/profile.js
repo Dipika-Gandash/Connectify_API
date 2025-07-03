4420139da8df1af0ac3523bec1d63499f345b7a7
Dipika Gandash <dipikadevv1445@gmail.com>
2025-07-03 12:33:26 +0530

    add secure password update route with validation, hashing, and old password check

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,9 @@ const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const { User } = require("../modals/userSchema")
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile", userAuth, async (req, res) => {
   try {
@@ -39,4 +42,50 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
   }
 });
 
+profileRouter.patch("/profile/update/password", userAuth, async (req, res) => {
+  try{
+    const { oldPassword, newPassword }= req.body;
+    const loggedInUser = req.user;
+
+    if( !oldPassword || !newPassword){
+      return res.status(400).send("Old password and new password are required");
+    }
+
+
+    const isOldPasswordValid = await loggedInUser.validatePassword(oldPassword);
+
+    if(!isOldPasswordValid){
+      return res.status(400).send("You old Password is incorrect");
+    }
+
+    if(oldPassword === newPassword){
+      return res.status(400).send("New password cannot be the same as old password");
+    }
+
+      if (!validator.isStrongPassword(newPassword, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })) {
+      return res.status(400).send("New password is not strong enough. It must be at least 8 characters long and include uppercase, lowercase, number, and special character.");
+    }
+
+
+    const newPasswordHash = await bcrypt.hash(newPassword, 10);
+
+    loggedInUser.password = newPasswordHash;
+    await loggedInUser.save();
+
+    res.status(200).json({
+      message : "Password updated successfully",
+    })
+
+
+
+  }catch(error){
+    return res.status(500).send(error.message);
+  }
+})
 module.exports = profileRouter;
