233a7e1289dc7eec4c826af9cbe23ee24a9d7380
Dipika Gandash <dipikadevv1445@gmail.com>
2025-07-06 19:41:33 +0530

    implement user feed with pagination logic

--- src/routes/connections.js
+++ src/routes/connections.js
@@ -68,6 +68,43 @@ connectionRouter.get('/user/connections', userAuth, async (req, res) => {
 })
 
 
+connectionRouter.get('/user/feed', userAuth, async (req, res) => {
+     const loggedInUser = req.user;
+     const page = req.query.page || 1;
+     const limit = req.query.limit || 5;
+     const skip = (page - 1) * limit;
+
+     try{
+          const connections = await ConnectionRequest.find({
+               $or : [
+                    {fromUserId: loggedInUser._id},
+                    {toUserId: loggedInUser._id}
+               ]
+          })
+
+          const hideUserFromFeed = new Set();
+          connections.forEach((row) => {
+               hideUserFromFeed.add(row.fromUserId.toString());
+               hideUserFromFeed.add(row.toUserId.toString());
+          })
+
+          const users = await User.find({
+               $and : [
+                    {_id: {$nin: Array.from(hideUserFromFeed)}},
+                    {_id: {$ne: loggedInUser._id}}
+               ]
+          }).select(USER_DATA).skip(skip).limit(limit);
+
+          return res.status(200).json({
+               message: 'Feed retrieved successfully',
+               data: users
+          })
+
+     }catch(error){
+          return res.status(500).send(error.message);
+     }
+})
+
 
 
 module.exports = connectionRouter;
